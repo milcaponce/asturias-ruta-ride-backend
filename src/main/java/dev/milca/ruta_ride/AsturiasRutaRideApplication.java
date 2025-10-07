e260fcdae80f5c2bd408e1c468b9316a5fd0dc63
milcaponce <milcaponce.dev@gmail.com>
2025-10-07 13:01:19 +0200

    Init: First commit backend

---
+++ src/main/java/dev/milca/ruta_ride/AsturiasRutaRideApplication.java
@@ -0,0 +1,13 @@
+package dev.milca.ruta_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsturiasRutaRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsturiasRutaRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/milca/ruta_ride/AsturiasRutaRideApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.milca.ruta_ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsturiasRutaRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
