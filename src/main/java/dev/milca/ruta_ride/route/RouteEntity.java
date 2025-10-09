72c7015ca27687d1a3c46f507718ec56968c5f68
milcaponce <milcaponce.dev@gmail.com>
2025-10-09 10:31:48 +0200

    feat(route): Create RouteEntity class

---
+++ src/main/java/dev/milca/ruta_ride/route/RouteEntity.java
@@ -0,0 +1,51 @@
+package dev.milca.ruta_ride.route;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "routes")
+public class RouteEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRoute;
+
+    private String name;
+    private String area;
+    private Integer kilometres;
+    private String difficulty;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    // ---Constructors ---
+    public RouteEntity() {}
+
+    public RouteEntity(String name, String area, Integer kilometres, String difficulty, String description) {
+        this.name = name;
+        this.area = area;
+        this.kilometres = kilometres;
+        this.difficulty = difficulty;
+        this.description = description;
+    }
+
+    // ---Getter y Setters ---
+    public Long getIdRoute() { return idRoute; }
+    public void setIdRoute(Long idRoute) {this.idRoute = idRoute; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getArea() { return area; }
+    public void setArea(String area) { this.area = area; }
+
+    public Integer getKilometres() { return kilometres; }
+    public void setKilometres(Integer kilometres) { this.kilometres = kilometres; }
+
+    public String getDifficulty() { return difficulty; }
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
