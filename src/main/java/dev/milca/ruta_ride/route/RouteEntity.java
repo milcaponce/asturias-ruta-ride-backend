a84dd5022d68fec8a215e611bbb433912e16d8a6
milcaponce <milcaponce.dev@gmail.com>
2025-10-09 11:49:20 +0200

    feat(route): Add basic RouteService with findAll method

--- src/main/java/dev/milca/ruta_ride/route/RouteEntity.java
+++ src/main/java/dev/milca/ruta_ride/route/RouteEntity.java
@@ -1,7 +1,11 @@
 package dev.milca.ruta_ride.route;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "routes")
